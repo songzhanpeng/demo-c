b172b2db26485fb719e0ba552f02e69c31eb61a9
songzhanpeng <songzhanpeng@iauto.com>
2024-02-28 20:10:30 +0800

    feat: 修改

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const ffi = require("ffi-napi");
 console.log("Before ffi.Library initialization");
 const parser = ffi.Library("./x84_64/libsomeip_parser_ics.so", {
   // 定义共享对象文件中的函数及其参数和返回值类型
-//   'loadFormatJson': ['void', ['string']],
+  'NESomeIPParserAPI': ['pointer', []], // MyClass构造函数
+  // 'loadFormatJson': ['int', ['string']],
 //   'deserializebyMethodID': ['string', ['int', 'int', 'int', 'uint8*', 'string*']]
 });
 console.log("After ffi.Library initialization");
 
+// 创建MyClass对象
+const objPtr = parser.NESomeIPParserAPI();
+if (!objPtr) throw new Error("Failed to create MyClass object");
 
 // console.log("Before calling loadFormatJson");
 // parser.loadFormatJson("./TotalJsonFormat.json");
