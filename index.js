8fa910b4ac0bbf8ad25fdf11a70c38ca05153950
songzhanpeng <songzhanpeng@iauto.com>
2024-02-28 14:35:10 +0800

    feat: 修改

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const ffi = require("ffi-napi");
 
-const hello = ffi.Library("./hello.so", {
-  hello: ["void", ["string"]],
+const parser = ffi.Library("./x84_64/libsomeip_parser_ics.so", {
+  // 定义共享对象文件中的函数及其参数和返回值类型
+  'loadFormatJson': ['void', ['string']],
+  'deserializebyMethodID': ['string', ['int', 'int', 'int', 'uint8*', 'string*']]
 });
 
-hello.hello("Alice");
+parser.loadFormatJson("./TotalJsonFormat.json");
